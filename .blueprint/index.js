b0dd1accd59e7e4522b598b59afb09e7cfedd633
neilswart3 <neilswart3@gmail.com>
2021-12-28 12:01:32 +0200

    install

---
+++ .blueprint/index.js
@@ -0,0 +1,73 @@
+const fs = require('fs')
+const { component, barrel, styles, story } = require('./templates.js')
+
+// grab component name from terminal argument
+const [type] = process.argv.slice(2)
+const [relPath] = process.argv.slice(3)
+const [name] = process.argv.slice(4)
+const [props] = process.argv.slice(5)
+
+if (!name) throw new Error('You must include a component name.')
+
+const fragments = relPath !== '.' ? `/${relPath}/fragments` : ''
+
+const dir = `./src/${type}${fragments}/${name}/`
+
+// throw an error if the file already exists
+if (fs.existsSync(dir))
+  throw new Error('A component with that name already exists.')
+
+// create the folder
+fs.mkdirSync(dir)
+
+function writeFileErrorHandler(err) {
+  if (err) throw err
+}
+
+// component.tsx
+fs.writeFile(`${dir}/index.tsx`, component(name, props), writeFileErrorHandler)
+// styles.ts
+fs.writeFile(`${dir}/styles.ts`, styles(name), writeFileErrorHandler)
+// storybook.jsx
+fs.writeFile(
+  `${dir}/${name}.stories.tsx`,
+  story(name, type),
+  writeFileErrorHandler
+)
+// // test.tsx
+// fs.writeFile(`${dir}/${name}.test.tsx`, test(name), writeFileErrorHandler);
+
+////////////////
+/// Optional ///
+////////////////
+
+// insert new component into 'components/index.ts file
+fs.readFile(`./src/${type}${fragments}/index.ts`, 'utf8', function (err, data) {
+  if (err) throw err
+
+  // grab all components and combine them with new component
+  const currentComponents = data.match(/(?<=import )(.*?)(?= from)/g) || null
+
+  let newComponents = [name]
+  if (currentComponents) {
+    newComponents = [name, ...currentComponents].sort()
+  }
+
+  // create the import and export statements
+  const importStatements = newComponents
+    .map((importName) => `import ${importName} from './${importName}'\n`)
+    .join('')
+  const exportStatements = `export {\n${newComponents
+    .map((component) => `  ${component},\n`)
+    .join('')}}\n`
+
+  const fileContent = `${importStatements}\n${exportStatements}`
+
+  fs.writeFile(
+    `./src/${type}${fragments}/index.ts`,
+    fileContent,
+    writeFileErrorHandler
+  )
+
+  console.log(`${name} has been created.`)
+})
---
+++ .blueprint/templates.js
@@ -0,0 +1,72 @@
+// component.tsx
+exports.component = (name, props) => `import React from 'react'
+import Styled from './styles'
+${
+  props
+    ? `
+interface Props {
+}
+`
+    : ''
+}
+const ${name}: React.FC${props ? `<Props>` : ''} = (${
+  props ? `props` : ''
+}) => {
+  return (
+    <Styled.${name}>${name} component</Styled.${name}>
+  );
+};
+export default ${name}
+`
+
+// component.stories.jsx
+exports.story = (name, type) => {
+  const typeArr = type.split('')
+  const folder = [typeArr.splice(0, 1), ...typeArr].join('')
+
+  return `import React from 'react'
+import ${name} from '.'
+
+export default {
+  title: '${folder}/${name}',
+  component: ${name},
+}
+
+export const Default = () => <${name} />
+`
+}
+
+// component.test.tsx
+exports.test = (name) => `import React from 'react'
+import { render } from '@testing-library/react'
+import ${name} from './${name}'
+
+const props = {
+
+}
+
+describe('${name} Component', () => {
+  test('it should match the snapshot', () => {
+    const { asFragment } = render(<${name} {...props} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
+`
+
+// styles.ts
+exports.styles = (name) => `import styled from '@emotion/styled'
+
+const ${name} = styled.div\`
+  display: block;
+\`
+
+const Styled = { ${name} }
+
+export default Styled
+`
+
+// index.ts
+exports.barrel = (name) => `import ${name} from './${name}'
+
+export default ${name}
+`
