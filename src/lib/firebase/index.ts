de10cc11f1c1cb6f6ba73bcc40531d83a7305565
neilswart3 <neilswart3@gmail.com>
2021-12-29 15:58:28 +0200

    default fa

--- src/lib/firebase/index.ts
+++ src/lib/firebase/index.ts
@@ -1,13 +1,15 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  // apiKey: process.env.API_KEY,
+  // authDomain: process.env.AUTH_DOMAIN,
+  // projectId: process.env.PROJECT_ID,
+  // storageBucket: process.env.STORAGE_BUCKET,
+  // messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  // appId: process.env.APP_ID,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+
+export default app
